8b2d751120134b89a1a10b1e8c893e31619a9cd1
ahmdsabbir <ahmd.sabbir@gmail.com>
2022-11-13 20:16:58 +0600

    initial commit

--- app.py
+++ app.py
@@ -12,5 +12,17 @@ def index():
 def profile(name):
     return render_template('user.html', name=name)
 
+# custom error page
+
+#invalid error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
