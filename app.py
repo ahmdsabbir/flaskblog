93736f12b81e642cd7437df7240317b4410aac1d
ahmdsabbir <ahmd.sabbir@gmail.com>
2022-11-14 17:20:20 +0600

    db integration

--- app.py
+++ app.py
@@ -1,9 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 # Flask instance
 app = Flask(__name__)
 
-# a route decorator
+app.config['SECRET_KEY'] = "my super secret key that no one else knows"
+# # old sqllite db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# new mysql db
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///usersname:password@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/our_users'
+
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Name {self.name}>'
+
+class UserForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# form class
+class NamerForm(FlaskForm):
+    name = StringField('name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added successfully')
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', form=form, our_users=our_users, name=name)
+
+# route decorator
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,6 +75,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+# Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validate
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+        flash('Form submitted successfully')
+    return render_template('name.html', name=name, form=form)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
